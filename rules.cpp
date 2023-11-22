0decfa662abdbcc87a5b6600559c6c434ac50283
Nick <145927489+ChungTau0222@users.noreply.github.com>
2023-11-23 02:11:20 +0800

    Update rules.cpp

--- rules.cpp
+++ rules.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 void rule(){
     cout << "Monopoly Rules" << endl;
-    cout << "--------------" << endl;
-    cout << "1. The game is played on a square board divided into 40 spaces." << endl;
+    cout << "-------------------------------------------------------------------------" << endl;
+    cout << "1. The game is played on a square board divided into 40 tiles." << endl;
     cout << "2. Each player starts with a set amount of money and a token on the 'GO' space." << endl;
     cout << "3. Players take turns in clockwise order." << endl;
     cout << "4. Roll two six-sided dice and move your token clockwise around the board." << endl;
     cout << "   - If you roll doubles, you roll again and take another turn." << endl;
     cout << "5. Depending on the space you land on, you can buy properties, pay rent, draw cards, etc." << endl;
-    cout << "6. If you land on an unowned property, you can choose to buy it. If not, it will be auctioned." << endl;
-    cout << "7. If you own all the properties of a color group, you can build houses and hotels on them." << endl;
+    cout << "6. If you land on an unowned property, you can choose to buy it. If not, there is nothing happen." << endl;
+    cout << "7. If you own the property, you can build houses and hotels on them when you land on it again" << endl;
     cout << "8. If you land on a property owned by another player, you must pay them rent." << endl;
-    cout << "9. If you land on certain spaces, you may draw a Chance or Community Chest card." << endl;
+    cout << "9. If you land on certain spaces, you may draw a Chance card." << endl;
     cout << "10. The game continues until all but one player goes bankrupt." << endl;
     cout << "11. The last remaining player is declared the winner of the game." << endl;
 }
