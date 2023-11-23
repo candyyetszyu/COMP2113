e55918aac67a1f67115dff80e5556df85355f329
raissayeung <102832521+raissayeung@users.noreply.github.com>
2023-11-24 02:29:16 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -14,7 +14,7 @@ struct Player {
     int money = 0; // amount of money the player has
     int position = 0; // position of the player on the board, integer between 0 and 39
     string name = ""; // name of player
-    bool in_prison = false; // whether player is in prison or not
+    bool in_jail = false; // whether player is in jail or not
     bool is_bot;
 };
 
@@ -57,6 +57,7 @@ void apply_chance_card_effect(Player& player, const ChanceCard& card, Tile tiles
         case ChanceCardType::GoToJail:
             cout << player.name << " goes to jail!" << endl;
             player.position = tiles[10].index; // Jail tile is always at index 10
+            player.in_jail = true; // Set the player in jail
             break;
 
         case ChanceCardType::GoBackThreeSpaces:
@@ -81,6 +82,12 @@ void apply_chance_card_effect(Player& player, const ChanceCard& card, Tile tiles
     }
 }
 
+void pay_fine(Player& player, int fine) {
+    player.money -= fine;
+    player.in_jail = false;
+    cout << player.name << " paid a fine of $" << fine << " and is released from jail." << endl;
+}
+
 int run_game(
         int n,
         vector<Player> players,
@@ -95,56 +102,70 @@ int run_game(
     random_shuffle(chance_card.begin(), chance_card.end());
 
     while (true) {
-        // roll dice
-        int dice1 = rand() % 6 + 1;
-        int dice2 = rand() % 6 + 1;
-        cout << players[i].name << " rolled " << dice1 << " and " << dice2 << " on the dice." << endl;
+        if (players[i].in_jail) {
+            // Player is in jail
+            cout << players[i].name << " is in jail and must wait for their next turn." << endl;
+            cout << "Do you want to pay the $100 fine to be released immediately? (yes/no)" << endl;
+            string choice;
+            cin >> choice;
+            
+            if (choice == "yes") {
+                pay_fine(players[i], 100);
+            } else {
+                cout << players[i].name << " chose not to pay the fine and will remain in jail." << endl;
+            }
+        } else {
+            // roll dice
+            int dice1 = rand() % 6 + 1;
+            int dice2 = rand() % 6 + 1;
+            cout << players[i].name << " rolled " << dice1 << " and " << dice2 << " on the dice." << endl;
 
-        // move player position
-        players[i].position = (players[i].position + dice1 + dice2) % tile_size;
-        cout << players[i].name << " moved to tile " << players[i].position << "." << endl;
+            // move player position
+            players[i].position = (players[i].position + dice1 + dice2) % tile_size;
+            cout << players[i].name << " moved to tile " << players[i].position << "." << endl;
 
-        // handle actions with tiles
-        switch (tiles[players[i].position].type) {
-            case 0: // chance
-                cout << players[i].name << " got a chance card!" << endl;
+            // handle actions with tiles
+            switch (tiles[players[i].position].type) {
+                case 0: // chance
+                    cout << players[i].name << " got a chance card!" << endl;
 
-                // apply chance card effect if available
-                if (!chance_card.empty()) {
-                    apply_chance_card_effect(players[i], chance_card.back(), tiles, free_parking, players);
-                    chance_card.pop_back(); // remove the last chance card
-                }
+                    // apply chance card effect if available
+                    if (!chance_card.empty()) {
+                        apply_chance_card_effect(players[i], chance_card.back(), tiles, free_parking, players);
+                        chance_card.pop_back(); // remove the last chance card
+                    }
 
-                break;
-            case 1: // community chest
-                cout << players[i].name << " got a community chest card!" << endl;
-                // TODO: Implement community chest card functionality
-                break;
-            case 2: // go to jail
-                cout << players[i].name << " went to jail!" << endl;
-                players[i].position = 10; // Go to Jail tile
-                players[i].in_prison = true;
-                break;
-            case 3: // free parking
-                cout << players[i].name << " moved to free parking and received $" << free_parking << "!" << endl;
-                players[i].money += free_parking;
-                break;
-            case 4: // property
-                // TODO: Implement property tile functionality
-                break;
-            case 5: // go
-                // TODO: Implement go tile functionality
-                break;
-            case 6: // tax
-                cout << players[i].name << " paid $" << tiles[players[i].position].name << " tax!" << endl;
-                players[i].money -= atoi(tiles[players[i].position].name.c_str());
-                break;
-            case 7: // railroad
-                // TODO: Implement railroad tile functionality
-                break;
-            case 8: // utility
-                // TODO: Implement utility tile functionality
-                break;
+                    break;
+                case 1: // community chest
+                    cout << players[i].name << " got a community chest card!" << endl;
+                    // TODO: Implement community chest card functionality
+                    break;
+                case 2: // go to jail
+                    cout << players[i].name << " went to jail!" << endl;
+                    players[i].position = 10; // Go to Jail tile
+                    players[i].in_jail = true; // Set the player in jail
+                    break;
+                case 3: // free parking
+                    cout << players[i].name << " moved to free parking and received $" << free_parking << "!" << endl;
+                    players[i].money += free_parking;
+                    break;
+                case 4: // property
+                    // TODO: Implement property tile functionality
+                    break;
+                case 5: // go
+                    // TODO: Implement go tile functionality
+                    break;
+                case 6: // tax
+                    cout << players[i].name << " paid $" << tiles[players[i].position].name << " tax!" << endl;
+                    players[i].money -= atoi(tiles[players[i].position].name.c_str());
+                    break;
+                case 7: // railroad
+                    // TODO: Implement railroad tile functionality
+                    break;
+                case 8: // utility
+                    // TODO: Implement utility tile functionality
+                    break;
+            }
         }
 
         if (i + 1 == n) {
@@ -161,4 +182,3 @@ int run_game(
         }
     }
 }
-
