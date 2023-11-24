7ace0bdeb7d288fba4bbc42011f3ea478c10150d
Nick <145927489+ChungTau0222@users.noreply.github.com>
2023-11-25 00:24:54 +0800

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include "board.h"
 
+using namespace std;
+
+void PrintBoard(const Player& player, const std::vector<Property>& properties){
+
+}
