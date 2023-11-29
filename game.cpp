2b7f9526313e6fca9c25723fae4cf14007d048de
Ye Tsz Yu <146556039+candyyetszyu@users.noreply.github.com>
2023-11-29 17:23:48 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -332,7 +332,7 @@ void Player::buyHouse(Tile& tile) {
 	if (tile.owner == position) {
 		int color_sum=0, houses_num=0, houses_num_of_same_color=0;
 		for (int i=0; i<39; i++){
-			if (tile[i].owner == tile.owner && tile[i].group==tile[position].color){
+			if (tile[i].owner == tile.owner && tile[i].group==tile[position].group){
 				color_sum+=1; //detect whether players hv all property of the group
 			}
 			if (tile[i].owner == tile.owner && tile[i].houses>=0){
@@ -343,7 +343,7 @@ void Player::buyHouse(Tile& tile) {
 				}
 			}
 		}
-		if (tile[position].color == 1 ||tile[position].color == 8){
+		if (tile[position].group == 1 ||tile[position].group == 8){
 			if (houses_num_of_same_color!=2 && tile[position].houses>=0){
 				std::cout << "You cannot buy houses here until you have built one house on every property of the group.\n";
 				return;
@@ -370,7 +370,7 @@ void Player::buyHouse(Tile& tile) {
 				}
 			}
 		}
-		else if (tile[position].color == 2 ||tile[position].color == 3||tile[position].color == 4 ||tile[position].color == 5||tile[position].color == 6 ||tile[position].color == 7){
+		else if (tile[position].group == 2 ||tile[position].group == 3||tile[position].group == 4 ||tile[position].group == 5||tile[position].group == 6 ||tile[position].group == 7){
 			if (houses_num_of_same_color!=3 && tile[position].houses>=0){
 				std::cout << "You cannot buy houses here until you have built one house on every property of the group.\n";
 				return;
